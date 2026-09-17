ccb1c6f8198279e064ca19a945e2a577cf44d956
Maximka <reschikov@list.ru>
2026-09-17 21:49:24 +0800

    feat: добавлено очистка лога при запуске скрипта и цветовой вывод сообщений в терминал

--- resource_monitoring.sh
+++ resource_monitoring.sh
@@ -4,16 +4,18 @@ readonly LOG_NAME="monitor.log"
 readonly DELAY=20
 readonly EXPECTATION=0.5
 
+> "$LOG_NAME"
+
 delay(){
     for ((i=0; i<DELAY; i++)); do
         if read -t "$EXPECTATION" -n 1 -s; then
-            echo "Результат в $LOG_NAME"
+            echo -e "Результат в \033[0;32m$LOG_NAME\033[0m"
             exit 0
         fi
     done 
 }
 
-echo "Для завершения нажать любую клавишу"
+echo -e "\033[0;33mДля завершения нажать любую клавишу\033[0m"
 while true; do
     (
         date "+--------------------  %Y-%m-%d %H:%M:%S  -----------------------"

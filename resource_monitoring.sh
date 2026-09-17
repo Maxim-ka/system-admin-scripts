ce9144583bdb007111008d6b502ed051a78b4dfc
Maximka <reschikov@list.ru>
2026-09-17 21:17:42 +0800

    fix: исправления логики, вынос задержки в отдельную функцию, форматирование вывода отчета

--- resource_monitoring.sh
+++ resource_monitoring.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
+
 readonly LOG_NAME="monitor.log"
 readonly DELAY=20
-echo "Для завершения нажать любую клавишу"
-while true; do
-    (
-        date "+----------  %Y-%m-%d %H:%M:%S  -----------"
-        free -h
-        2 -h
-        uptime
-    ) >> "$LOG_NAME" 2>&1
-    for ((i=0; i<=DELAY; i++)); do
-        if read -t 0.5 -n 1 -s; then
+readonly EXPECTATION=0.5
+
+delay(){
+    for ((i=0; i<DELAY; i++)); do
+        if read -t "$EXPECTATION" -n 1 -s; then
+            echo "Результат в $LOG_NAME"
             exit 0
         fi
     done 
+}
+
+echo "Для завершения нажать любую клавишу"
+while true; do
+    (
+        date "+--------------------  %Y-%m-%d %H:%M:%S  -----------------------"
+        free -h
+        printf "\n\n"
+        df -h
+        printf "\n\n"
+	uptime
+        echo "----------------------------------------------------------------"
+        printf "\n\n"
+    ) >> "$LOG_NAME" 2>&1
+    delay
 done
-echo "Результат в $LOG_NAME"
+
